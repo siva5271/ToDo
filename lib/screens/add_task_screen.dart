c7e08bd40b6c6c7a340fa5c5c3c4570edfca6c5d
Siva <71comingsoon@gmail.com>
2022-10-16 10:32:53 +0530

    complete the interface

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -10,19 +10,51 @@ class AddTaskScreen extends StatelessWidget {
             color: Colors.white,
             borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(20), topRight: Radius.circular(20))),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text(
-              'Add Task',
-              style: TextStyle(
-                  color: Colors.lightBlueAccent,
-                  fontSize: 25,
-                  fontWeight: FontWeight.w600),
-            ),
-            TextField(),
-            TextButton(onPressed: () {}, child: Text('Add'))
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Add Task',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.lightBlueAccent,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600),
+              ),
+              TextField(
+                autofocus: true,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.lightBlueAccent, width: 3)),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.blueGrey, width: 5.0)),
+                    labelText: 'Enter the task'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.lightBlueAccent,
+                ),
+                onPressed: () {},
+                child: Expanded(
+                  child: Text(
+                    'Add',
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
